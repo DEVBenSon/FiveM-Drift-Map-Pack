f8ebea5992158f31af61b54923079bf3c68e2ec3
BenSon <contact@cmurphy.dev>
2020-05-27 15:06:59 +0100

    Added New Maps
    Akagipass & Nordschleife

---
+++ -[ PreConfigured Packs ]-/nordschleife_map/__resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+this_is_a_map 'yes'
+
+client_scripts {
+	"NurburgringNordschleife_cl.lua"
+}
+
+files {
+    'data/gtxd.meta'
+}
+
+data_file 'GTXD_PARENTING_DATA' 'data/gtxd.meta'
